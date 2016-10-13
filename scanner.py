9bcac8a6767edaf3f01cc67c898dc4d3ce49e391
Kevin <kevoestrada@gmail.com>
2016-10-13 17:10:33 -0500

    avance

--- scanner.py
+++ scanner.py
@@ -28,37 +28,44 @@ keywords = {
     'falso': 'KW_FALSO',
     'booleano': 'KW_BOOLEANO',
     'lista': 'KW_LISTA',
+    'en': 'KW_EN',
 }
 
 tokens = {
     'KW_ARRIBA','KW_IZQUIERDA','KW_DERECHA', 'KW_BORRAR', 'KW_MIENTRAS', 'KW_REPETIR',
     'KW_DIBUJASI', 'KW_DIBUJANO', 'KW_COLOR', 'KW_CUANDO', 'KW_FIN', 'KW_CIRCULO', 'KW_CUADRADO',
-    'KW_RECTANGULO', 'KW_TRIANGULO', 'KW_LINEA', 'KW_ENTERO', 'KW_PALABRA',
+    'KW_RECTANGULO', 'KW_TRIANGULO', 'KW_LINEA', 'KW_ENTERO', 'KW_PALABRA', 'KW_EN',
     'EQUALS', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'POWER',
-    'LPAREN', 'RPAREN', 'LT', 'LE', 'GT', 'GE', 'NE',
-    'COMMA', 'SEMI', 'COLON', 'INTEGER', 'FLOAT', 'STRING',
+    'LPAREN', 'RPAREN', 'LT', 'LE', 'GT', 'GE', 'NE', 
+    'COMMA', 'SEMI', 'COLON', 'INTEGER', 'FLOAT', 'STRING', 'LCURLY', 'RCURLY', 'LBRACKET', 'RBRACKET',
     'ID', 'NEWLINE'
 }
 
-t_EQUALS = r'='
+t_EQUALS = r'[=]'
 t_PLUS = r'\+'
-t_MINUS = r'-'
+t_MINUS = r'\-'
 t_TIMES = r'\*'
 t_POWER = r'\^'
-t_DIVIDE = r'/'
+t_DIVIDE = r'\/'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
-t_LT = r'<'
-t_LE = r'<='
-t_GT = r'>'
-t_GE = r'>='
-t_NE = r'<>'
+t_LT = r'[<]'
+t_LE = r'[<=]'
+t_GT = r'[>]'
+t_GE = r'[>=]'
+t_NE = r'[<>]'
 t_COMMA = r'\,'
 t_SEMI = r';'
 t_COLON = r':'
 t_INTEGER = r'\d+'
 t_FLOAT = r'((\d*\.\d+)(E[\+-]?\d+)?|([1-9]\d*E[\+-]?\d+))'
 t_STRING = r'\".*?\"'
+t_LCURLY = r'\{'
+t_RCURLY = r'\}'
+t_LBRACKET = r'\['
+t_RBRACKET = r'\]'
+t_AND = r'[&][&]'
+t_OR = r'[|][|]'
 
 def t_CTE_F(t):
     r'[0-9]+\.[0-9]+'
@@ -124,17 +131,17 @@ def p_variables_aux1(p):
 def p_variables_aux2(p):
     """
     variables_aux2 : variables
-                 |
+                     |
     """
 
 def p_variables_aux3(p):
     """variables_aux3 : lista
-                 |
+                        |
     """
     
 def p_variables_aux4(p):
     """variables_aux4 : COMMA variables_aux1
-                 |
+                        |
     """
 
 def p_funciones(p):
@@ -149,13 +156,13 @@ def p_funciones_aux1(p):
 
 def p_funciones_aux2(p):
     """funciones_aux2 : funciones
-                 |
+                        |
     """
 
 def p_funciones_aux3(p):
     """
     funciones_aux3 : COMMA funciones_aux1
-              |
+                     |
     """
 def p_lista(p):
     """
@@ -178,11 +185,132 @@ def p_bloque(p):
 def p_bloque_aux1(p):
     """
     bloque_aux1 : bloque_aux2
-                   |
+                  |
     """
 
 def p_bloque_aux2(p):
     """
     bloque_aux2 : estatuto bloque_aux2
-                    |
+                  |
+    """
+
+def p_estatuto(p):
+    """
+    estatuto : asignacion
+               | condicion
+               | accion
+               | dibujo
+               | mientras
+               | paratodo
+    """
+
+def p_asignacion(p):
+    """
+    asignacion : ID aignacion_aux1 EQUALS expresion SEMI
+    """
+
+def p_asignacion_aux1(p):
+    """
+    asignacion_aux1 : LBRACKET exp RBRACKET
+    """
+
+def p_condicion(p):
+    """
+    condicion : KW_CUANDO LPAREN expresion RPAREN bloque condicion_aux1
+    """
+
+def p_condicion_aux1(p):
+    """
+    condicion_aux1 : KW_SINO bloque
+    |
+    """
+def p_expresion(p):
+    """
+    expresion : exp expresion_aux1
+    """
+
+def p_expresion_aux1(p):
+    """
+    expresion_aux1 : expresion_aux2 exp
+    """
+
+def p_expresion_aux2(p):
+    """
+    expresion_aux2 : AND
+                     | OR
+    """
+
+def p_exp(p):
+    """
+    exp : termino exp_aux1
+    """
+
+def p_exp_aux1(p):
+    """
+    exp_aux1 : exp_aux2 termino
+    """
+
+def p_exp_aux2(p):
+    """
+    exp_aux2 : PLUS
+               | MINUS
+    """
+
+def p_termino(p):
+    """
+    termino : factor termino_aux1
+    """
+def p_termino_aux1(p):
+    """
+    termino_aux1 : termino_aux2 factor
+    """
+
+def p_termino_aux2(p):
+    """
+    termino_aux2 : TIMES
+                   | DIVIDE
+    """
+
+def p_factor(p):
+    """
+    factor : LPAREN expresion RPAREN
+    | cte
+    """
+
+def p_cte(p):
+    """
+    cte : ID cte_aux1
+          | CTE_E
+          | CTE_F
+    """
+
+def p_cte_aux1(p):
+    """
+    cte_aux1 : LBRACKET exp RBRACKET
+               | LPAREN exp RPAREN
+               |
+    """
+
+def p_accion(p):
+    """
+    accion : accion_aux1 LPAREN exp RPAREN
+    """
+
+def p_accion_aux1(p):
+    """
+    accion_aux1 : arriba
+                  | abajo
+                  | izquierda
+                  | derecha
+                  | color
+    """
+
+def p_mientras(p):
+    """
+    mientras : KW_MIENTRAS LPAREN expresion RPAREN bloque
+    """
+
+def p_paratodos(p):
+    """
+    paratodos: KW_PARATODOS LPAREN ID KW_EN lista RPAREN bloque
     """
