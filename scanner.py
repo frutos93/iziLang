4c228203b59fbf81c2ef1fc31f581fddb34895c2
Kevin <kevoestrada@gmail.com>
2016-10-13 14:54:57 -0500

    avance

--- scanner.py
+++ scanner.py
@@ -1,5 +1,5 @@
-from ply import *
-
+import ply.lex as lex
+import ply.yacc as yacc
 
 keywords = {
     'arriba': 'KW_ARRIBA',
@@ -36,18 +36,9 @@ tokens = {
     'KW_RECTANGULO', 'KW_TRIANGULO', 'KW_LINEA', 'KW_ENTERO', 'KW_PALABRA',
     'EQUALS', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'POWER',
     'LPAREN', 'RPAREN', 'LT', 'LE', 'GT', 'GE', 'NE',
-    'COMMA', 'SEMI', 'INTEGER', 'FLOAT', 'STRING',
+    'COMMA', 'SEMI', 'COLON', 'INTEGER', 'FLOAT', 'STRING',
     'ID', 'NEWLINE'
 }
-
-t_ignore = ' \t'
-
-
-def t_ID(t):
-    r'[A-Z][A-Z0-9]*'
-    if t.value in keywords:
-        t.type = t.value
-    return t
 
 t_EQUALS = r'='
 t_PLUS = r'\+'
@@ -64,19 +55,134 @@ t_GE = r'>='
 t_NE = r'<>'
 t_COMMA = r'\,'
 t_SEMI = r';'
+t_COLON = r':'
 t_INTEGER = r'\d+'
 t_FLOAT = r'((\d*\.\d+)(E[\+-]?\d+)?|([1-9]\d*E[\+-]?\d+))'
 t_STRING = r'\".*?\"'
 
+def t_CTE_F(t):
+    r'[0-9]+\.[0-9]+'
+    t.value = float(t.value)
+    return t
+
+
+def t_CTE_E(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_ID(t):
+    r'[A-Z][A-Z0-9]*'
+    if t.value in keywords:
+        t.type = t.value
+    return t
 
 def t_NEWLINE(t):
     r'\n'
     t.lexer.lineno += 1
     return t
 
-
 def t_error(t):
     print("Illegal character %s" % t.value[0])
     t.lexer.skip(1)
 
+t_CTE_S = r'\"[A-Za-z0-9_\(\)\{\}\[\]\<\>\! \t]*\"'
+
+t_ignore = ' \t'
+
 lex.lex(debug=0)
+
+lexer = lex.lex()
+
+def p_programa(p):
+    """
+    programa : KW_PROGRAMA ID COLON programa_aux1 programa_aux2 bloque FIN
+    """
+
+def p_programa_aux1(p):
+    """
+    programa_aux1 : variables
+                    |
+    """
+
+def p_programa_aux2(p):
+    """
+    programa_aux2 : funciones
+                    |
+    """
+
+def p_variables(p):
+    """
+    variables : tipo variables_aux1 SEMI variables_aux2
+    """"
+
+def p_variables_aux1(p):
+    """
+    variables_aux1 : ID variables_aux3 variables_aux4
+    """
+
+def p_variables_aux2(p):
+    """
+    variables_aux2 : variables
+                 |
+    """
+
+def p_variables_aux3(p):
+    """variables_aux3 : lista
+                 |
+    """
+    
+def p_variables_aux4(p):
+    """variables_aux4 : COMMA variables_aux1
+                 |
+    """
+
+def p_funciones(p):
+    """
+    funciones : FUNCION tipo ID LPAREN funciones_aux1 RPAREN bloque funciones_aux2
+    """
+
+def p_funciones_aux1(p):
+    """
+    funciones_aux1 : tipo ID funciones_aux3
+    """
+
+def p_funciones_aux2(p):
+    """funciones_aux2 : funciones
+                 |
+    """
+
+def p_funciones_aux3(p):
+    """
+    funciones_aux3 : COMMA funciones_aux1
+              |
+    """
+def p_lista(p):
+    """
+    lista : LBRACKET cte RBRACKET
+    """
+
+def p_tipo(p):
+    """
+    tipo : entero
+           | decimal
+           | palabra
+           | booleano
+    """
+
+def p_bloque(p):
+    """
+    bloque : LCURLY bloque_aux RCURLY
+    """
+
+def p_bloque_aux1(p):
+    """
+    bloque_aux1 : bloque_aux2
+                   |
+    """
+
+def p_bloque_aux2(p):
+    """
+    bloque_aux2 : estatuto bloque_aux2
+                    |
+    """
